0ae58f0604d57c444c0080b552d1b9b404a01df2
Milan Rawat <milanrawat086@gmail.com>
2022-09-09 15:20:35 +0530

    logout backend

--- server.js
+++ server.js
@@ -76,7 +76,22 @@ io.on("connection", (socket) => {
     io.to(room).emit("room-messages", roomMessages);
     socket.broadcast.emit("notifications", room);
   });
-  
+
+  app.delete('/logout', async(req, res)=> {
+    try {
+      const {_id, newMessages} = req.body;
+      const user = await User.findById(_id);
+      user.status = "offline";
+      user.newMessages = newMessages;
+      await user.save();
+      const members = await User.find();
+      socket.broadcast.emit('new-user', members);
+      res.status(200).send();
+    } catch (e) {
+      console.log(e);
+      res.status(400).send()
+    }
+  })
 });
 
 server.listen(PORT, () => {
